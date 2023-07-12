0739925aa0f71df1bad80e5c79520c5ed3ba0821
Pablo Guinea <48864366+pabloguineab@users.noreply.github.com>
2023-07-12 13:39:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,44 +5,39 @@ import numpy as np
 from streamlit_option_menu import option_menu
 
 st.set_page_config(
-    page_title="Flowers Recognition",
+    page_title="Image Recognition",
     layout="centered",
     initial_sidebar_state="collapsed"
 )
 
 @st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('flower-recog-cnn.h5')
+def load_model(model_path):
+    model = tf.keras.models.load_model(model_path)
     return model
 
 IMAGE_SIZE = 224
 CHANNELS = 3
 
-class_indices = {'daisy': 0, 'dandelion': 1, 'rose': 2, 'sunflower': 3, 'tulip': 4}
-class_names = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+class_indices_flower = {'daisy': 0, 'dandelion': 1, 'rose': 2, 'sunflower': 3, 'tulip': 4}
+class_names_flower = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 
-st.header("Flowers Recognition")
+class_indices_dog = {'beagle': 0, 'chihuahua': 1, 'doberman': 2, 'french_bulldog': 3, 'golden_retriever': 4, 'malamute': 5, 'pug': 6, 'saint_bernard': 7, 'scottish_deerhound': 8, 'tibetan_mastiff': 9}
+class_names_dog = list(class_indices_dog.keys())
 
-
-def homepage():
+def homepage(title, link, examples):
+    st.header(title)
     st.subheader("Dataset")
     with st.expander("Link"):
-        st.write("[Kaggle](https://www.kaggle.com/datasets/alxmamaev/flowers-recognition)")
+        st.write(f"[Kaggle]({link})")
 
     with st.expander("Examples"):
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.image("datasamples/daisy.jpg", width=200, caption="daisy")
-        with col2:
-            st.image("datasamples/dandelion.jpg", width=200, caption="dandelion")
-        with col3:
-            st.image("datasamples/rose.jpg", width=250, caption="rose")
-        col4, col5, col6 = st.columns(3)
-        with col4:
-            st.image("datasamples/sunflower.jpg", width=200, caption="sunflower")
-        with col5:
-            st.image("datasamples/tulip.jpg", width=200, caption="tulip")
-    
+        for i in range(0, len(examples), 3):
+            cols = st.columns(3)
+            for j in range(3):
+                if i + j < len(examples):
+                    with cols[j]:
+                        st.image(f"datasamples/{examples[i+j]}.jpg", width=200, caption=examples[i+j])
+
     with st.expander("Splits"):
         st.write("**Overall Samples:** 4318")
         st.write("**Training:** 70%")
@@ -82,7 +77,6 @@ for layer in mobile_net.layers:
         st.code(code, language='python')
 
     with st.expander("Final Model"):
-
         code = """
         model = Sequential([
         mobile_net,
@@ -115,20 +109,17 @@ results = model.evaluate(test_batches)
     st.write("**Testing Loss:**","0.32296934723854065")
     st.write("**Testing Accuracy:**","0.8812785148620605")
 
-
-
-def try_model():
+def try_model(model, class_names):
     with st.spinner("Loading Model"):
-        model = load_model()
-    uploaded_file = st.file_uploader("Choose an Image", type=['png', 'jpg', 'JPEG'])
-    if st.button("Submit"):
-        if uploaded_file:
-            image = Image.open(uploaded_file)
-            st.image(image, caption="Uploaded Image", width=300)
-            with st.spinner("Predicting"):
-                pred = predict(image, model)
-                predicted_class = class_names[np.argmax(pred)]
-            st.success(f"I think the flower is **{predicted_class}**")
+        uploaded_file = st.file_uploader("Choose an Image", type=['png', 'jpg', 'JPEG'])
+        if st.button("Submit"):
+            if uploaded_file:
+                image = Image.open(uploaded_file)
+                st.image(image, caption="Uploaded Image", width=300)
+                with st.spinner("Predicting"):
+                    pred = predict(image, model)
+                    predicted_class = class_names[np.argmax(pred)]
+                st.success(f"I think the image is a **{predicted_class}**")
 
 def predict(image, model):
     data = np.ndarray(shape=(1, IMAGE_SIZE, IMAGE_SIZE, CHANNELS), dtype=np.float32)
@@ -140,25 +131,26 @@ def predict(image, model):
     prediction = model.predict(data)
     return prediction
 
-
 def launch():
-    
     selected = option_menu(
     menu_title=None,
-    options = ["Code", "Try Model", "About"],
-    icons= ["code-slash","play", "info"],
+    options = ["Flower Recognition Code", "Try Flower Recognition Model", "Dog Breeds Recognition Code", "Try Dog Breeds Recognition Model", "About"],
+    icons= ["code-slash","play", "code-slash","play", "info"],
     menu_icon="list",
     default_index=0,
     orientation="horizontal"
     )
 
-    if selected == "Code":
-        homepage()
-    if selected == "Try Model":
-        try_model()
+    if selected == "Flower Recognition Code":
+        homepage("Flowers Recognition", "https://www.kaggle.com/datasets/alxmamaev/flowers-recognition", list(class_indices_flower.keys()))
+    if selected == "Try Flower Recognition Model":
+        try_model(load_model('flower-recog-cnn.h5'), class_names_flower)
+    if selected == "Dog Breeds Recognition Code":
+        homepage("Dog Breeds Recognition", "https://www.kaggle.com/jessicali9530/stanford-dogs-dataset", list(class_indices_dog.keys()))
+    if selected == "Try Dog Breeds Recognition Model":
+        try_model(load_model('dog-breeds-recog-cnn.h5'), class_names_dog)
     if selected == "About":
         about()
-    
 
 def about():
     st.write("### ADVANCED MATHS DL")
